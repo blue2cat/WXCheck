09238efee2f3eae1ff0d8a50d2d7c7a18ddcdf36
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-11 14:40:38 -0700

    package layout

---
+++ bin/main.py
@@ -0,0 +1,14 @@
+#programatic analisis 
+
+# python imports
+import datetime
+import logging
+import optparse
+import sys
+import time
+
+#wxcheck imports
+from db_connect import *
+
+#start the thread
+my_function()
