97edf92327e1b262d97694719d54c3105602be91
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-11 20:49:36 -0700

    cal avg temp for 4/11 over the last 100 years

--- bin/db_connect.py
+++ bin/db_connect.py
@@ -1,2 +1,6 @@
-def my_function():
-  print("Hello from a function")
+#this file contans functions for database connections
+def mongodb_connect(db_name, password, db_url):
+    #create a pymongo connection
+    
+    client = pymongo.MongoClient("mongodb+srv://dataconnect:<password>@cluster0.jligz.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.test
--- bin/main.py
+++ bin/main.py
@@ -1,14 +1,28 @@
 #programatic analisis 
 
 # python imports
-import datetime
-import logging
-import optparse
-import sys
-import time
+import csv
 
 #wxcheck imports
-from db_connect import *
+#from db_connect import *
 
-#start the thread
-my_function()
+
+import csv
+total = int(0)
+c = int(0)
+with open('2540765.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+
+    for row in reader:
+        row_date = row[5][5:]
+        if row_date == '04-11':
+            if row[18] != "":
+                temp = int(row[18])
+                c = c + 1
+                print(row[5])
+                print(row[18])
+                total = total + temp
+
+
+print(total/c)
+
